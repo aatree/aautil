5b0075e9df2532920f0139f843965f114557877e
Bill La Forge <laforge49@gmail.com>
2016-01-20 18:04:27 -0500

    closer-test.clj works

---
+++ test/clj/aautil/closer_test.clj
@@ -0,0 +1,18 @@
+(ns aautil.closer-test
+  (:require [aautil.closer :refer :all]))
+
+(set! *warn-on-reflection* true)
+
+(close-components {})
+
+(defn close-a [this] (println "  close a"))
+(defn close-b [this] (println "  close b"))
+(defn close-c [this] (println "  close c"))
+
+(let [this (open-component {} "a" close-a)
+      this (open-component this "b" close-b)
+      this (open-component this "c" close-c)]
+  (println "first close")
+  (close-components this)
+  (println "second close")
+  (close-components this))
